d03bedde5f52dfb0d312c13c40bcef2a32524679
kushal717 <kvskushal98@gmail.com>
2020-07-19 22:39:26 +0530

    str

--- getdatatype.py
+++ getdatatype.py
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
-x=5
-=======
-x=5
->>>>>>> 361eeb67d65781e93f95234df5dd1cba17bc2f6d
-print(type(x))
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
